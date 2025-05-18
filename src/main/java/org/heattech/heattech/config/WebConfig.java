3ea0345549037947a5cbb5e1ba52cc4b3934fc7f
LEE <sw09112@yonsei.ac.kr>
2025-05-18 16:28:54 +0900

    CORS

--- src/main/java/org/heattech/heattech/config/WebConfig.java
+++ src/main/java/org/heattech/heattech/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 API 경로에 대해
-                .allowedOrigins("http://localhost:3000") // 프론트 주소
+                .allowedOrigins("http://localhost:3000", "https://heattech-yonsei.shop") // 프론트 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // 쿠키 사용 허용 시
